e43d01fca98e5b855447a54c6431e08c55bd2998
danielcrubens <dcr.tecnologi@gmail.com>
2023-09-25 20:43:42 -0300

    estruturando home :fire:

--- components/organisms/BookList.vue
+++ components/organisms/BookList.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
-    <HomeTemplate/>
+  <div class="book-list">
+<h2>LIstagem de livros</h2>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend( {
-layout:'ibook'
 })
 </script>
